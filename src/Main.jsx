84fc3f9c9b031430d49cf7df226e4ae5f65f41da
yuni <hys990730@gmail.com>
2024-06-08 18:41:09 +0900

    feat: Main 컴포넌트 할 일 목록 표시 기능 구현

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,29 +1,40 @@
-// src/main.jsx
-import React from "react";
-import TodoForm from "./TodoForm.jsx";
+// src/Main.jsx
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import addTodo from "./todoUtils/addTodo";
+import toggleComplete from "./todoUtils/toggleComplete";
+import deleteTodo from "./todoUtils/deleteTodo";
 
 const Main = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (todo) => {
+    setTodos((prevTodos) => addTodo(prevTodos, todo));
+  };
+
+  const handleToggleComplete = (index) => {
+    setTodos((prevTodos) => toggleComplete(prevTodos, index));
+  };
+
+  const handleDeleteTodo = (index) => {
+    setTodos((prevTodos) => deleteTodo(prevTodos, index));
+  };
+
   return (
     <div>
       <h1>할 일 목록</h1>
+      <p> Enter 키나 추가 버튼을 사용하여 할 일을 목록에 추가하세요.</p>
 
       {/* 할 일 입력 폼 */}
-      <TodoForm />
+      <TodoForm addTodo={handleAddTodo} />
 
       {/* 할 일 목록 표시 */}
-      <ul>
-        {/* 할 일 항목 */}
-        <li>
-          <span>예제 할 일 1</span>
-          <button>완료</button>
-          <button>삭제</button>
-        </li>
-        <li>
-          <span>예제 할 일 2</span>
-          <button>완료</button>
-          <button>삭제</button>
-        </li>
-      </ul>
+      <TodoList
+        todos={todos}
+        toggleComplete={handleToggleComplete}
+        deleteTodo={handleDeleteTodo}
+      />
     </div>
   );
 };
