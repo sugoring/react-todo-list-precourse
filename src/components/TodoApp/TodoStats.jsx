08b68ea4b1e0fea7f16baa1e9de891e7b515b137
yuni <hys990730@gmail.com>
2024-06-10 01:07:06 +0900

    refactor: TodoStats 컴포넌트를 역할별로 분리
    - 총 할 일 개수를 계산하고 렌더링하는 TotalTodos 컴포넌트 추가
    - 완료된 할 일의 개수를 계산하고 렌더링하는 CompletedTodos 컴포넌트 추가
    - 진행 중인 할 일의 개수를 계산하고 렌더링하는 ActiveTodos 컴포넌트 추가

--- src/components/TodoApp/TodoStats.jsx
+++ src/components/TodoApp/TodoStats.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import TotalTodos from "./TodoStats/TotalTodos";
+import CompletedTodos from "./TodoStats/CompletedTodos";
+import ActiveTodos from "./TodoStats/ActiveTodos";
 
-const TodoStats = ({ todos, allTodos, filter }) => {
+const TodoStats = ({ todos, allTodos }) => {
   const totalTodos = allTodos.length;
   const completedTodos = allTodos.filter((todo) => todo.completed).length;
   const activeTodos = totalTodos - completedTodos;
 
   return (
     <div>
-      총 할 일 개수: {totalTodos} | 완료된 할 일: {completedTodos} | 진행 중인
-      할 일: {activeTodos}
+      <TotalTodos total={totalTodos} /> |{" "}
+      <CompletedTodos completed={completedTodos} /> |{" "}
+      <ActiveTodos active={activeTodos} />
     </div>
   );
 };
---
+++ src/components/TodoApp/TodoStats/TotalTodos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TotalTodos = ({ total }) => {
+  return <span>총 할 일 개수: {total}</span>;
+};
+
+export default TotalTodos;
