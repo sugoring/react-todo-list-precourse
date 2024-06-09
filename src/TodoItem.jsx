1c8a611e43840b4f3f47314bbd2c440095f4ea9d
yuni <hys990730@gmail.com>
2024-06-09 19:27:27 +0900

    refactor: TodoItem 컴포넌트 index 대신 todo.id 사용

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const TodoItem = ({ todo, index, toggleComplete, deleteTodo }) => {
+// 개별 할 일 항목을 표시하는 컴포넌트
+const TodoItem = ({ todo, toggleComplete, deleteTodo }) => {
   return (
     <li style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
       {todo.text}
-      <button onClick={() => toggleComplete(index)}>
+      <button onClick={() => toggleComplete(todo.id)}>
         {todo.completed ? "미완료" : "완료"}
       </button>
-      <button onClick={() => deleteTodo(index)}>삭제</button>
+      <button onClick={() => deleteTodo(todo.id)}>삭제</button>
     </li>
   );
 };
