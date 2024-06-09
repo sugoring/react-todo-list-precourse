a7fc50185d57f7474f988aa09d5ae718c5ba25ea
yuni <hys990730@gmail.com>
2024-06-10 00:35:37 +0900

    refactor: useTodosState 훅을 상태별로 분리
    - 할 일 목록 상태 관리를 위한 useTodoList 훅 추가 (hooks/useTodoList.js)
    - 필터 상태 관리를 위한 useFilterState 훅 추가 (hooks/useFilterState.js

--- src/hooks/useTodosState.js
+++ src/hooks/useTodosState.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import FILTERS from "../todoUtils/filters";
+import useTodoList from "./useTodoList";
+import useFilterState from "./useFilterState";
 
 // 할 일 목록과 필터 상태를 관리하는 훅
 const useTodosState = () => {
-  const [todos, setTodos] = useState([]);
-  const [filter, setFilter] = useState(FILTERS.ALL);
+  const { todos, setTodos } = useTodoList();
+  const { filter, setFilter } = useFilterState();
 
   return {
     todos,
