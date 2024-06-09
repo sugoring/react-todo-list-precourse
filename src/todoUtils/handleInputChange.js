340d73dd8fd30831f52e186c427fac81bbc4ed1f
yuni <hys990730@gmail.com>
2024-06-09 19:21:28 +0900

    refactor: handleInputChange 함수 리팩토링 및 주석 추가
    - 함수에 주석을 추가하여 목적과 매개변수 설명
    - 매개변수 이름을 명확하게 변경 (event)

--- src/todoUtils/handleInputChange.js
+++ src/todoUtils/handleInputChange.js
@@ -1,6 +1,10 @@
-// Input 값 변경 핸들러 함수
-const handleInputChange = (e, setInputValue) => {
-  setInputValue(e.target.value);
+/**
+ * Input 값 변경 핸들러 함수
+ * @param {Event} event - Input 값 변경 이벤트
+ * @param {Function} setInputValue - 입력 필드 값을 설정하는 함수
+ */
+const handleInputChange = (event, setInputValue) => {
+  setInputValue(event.target.value);
 };
 
 export default handleInputChange;
