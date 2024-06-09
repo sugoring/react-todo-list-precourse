63c0bf0da1be276637045a5ba4c17dff50f85b92
yuni <hys990730@gmail.com>
2024-06-09 18:07:59 +0900

    feat: 필터링 기능 구현
    - 필터링 상태를 관리하는 로직을 useTodos 훅에 추가

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -3,8 +3,15 @@ import addTodo from "../todoUtils/addTodo";
 import toggleComplete from "../todoUtils/toggleComplete";
 import deleteTodo from "../todoUtils/deleteTodo";
 
+const FILTERS = {
+  ALL: "all",
+  ACTIVE: "active",
+  COMPLETED: "completed",
+};
+
 const useTodos = () => {
   const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState(FILTERS.ALL);
 
   const handleAddTodo = (todo) => {
     setTodos((prevTodos) => addTodo(prevTodos, todo));
@@ -18,11 +25,24 @@ const useTodos = () => {
     setTodos((prevTodos) => deleteTodo(prevTodos, index));
   };
 
+  const handleSetFilter = (filter) => {
+    setFilter(filter);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === FILTERS.ACTIVE) return !todo.completed;
+    if (filter === FILTERS.COMPLETED) return todo.completed;
+    return true;
+  });
+
   return {
-    todos,
+    todos: filteredTodos,
     handleAddTodo,
     handleToggleComplete,
     handleDeleteTodo,
+    handleSetFilter,
+    filter,
+    FILTERS,
   };
 };
 
