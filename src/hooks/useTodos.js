58a15ace51bd117b170cbec792dac6e116eda7f1
yuni <hys990730@gmail.com>
2024-06-10 01:50:47 +0900

    feat: 로컬 스토리지에서 할 일 목록 불러오는 기능 추가
    - 애플리케이션 초기화 시 로컬 스토리지에 저장된 할 일 목록을 불러옵니다.

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import useTodoStateManager from "./useTodoStateManager";
 import useTodoHandlers from "./useTodoHandlers";
 import FILTERS from "../todoUtils/filters";
 
+// 모든 상태와 핸들러를 결합하는 훅
 const useTodos = () => {
   const { todos, allTodos, setTodos, filter, setFilter } =
     useTodoStateManager();
   const handlers = useTodoHandlers(allTodos, setTodos, setFilter);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(storedTodos);
+  }, [setTodos]);
 
   return {
     todos,
