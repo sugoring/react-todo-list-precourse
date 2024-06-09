9bcfffe2f3085ed5092e3ee9a145711a2625a09a
yuni <hys990730@gmail.com>
2024-06-09 19:20:28 +0900

    refactor: handleSubmit 함수 리팩토링 및 주석 추가
    - 함수에 주석을 추가하여 목적과 매개변수 설명
    - 매개변수 이름을 명확하게 변경 (event, todoText, resetInput)

--- src/todoUtils/handleSubmit.js
+++ src/todoUtils/handleSubmit.js
@@ -1,14 +1,21 @@
-// 폼 제출 핸들러 함수
+// src/todoUtils/handleSubmit.js
 import validateTodo from "./validateTodo";
 
-const handleSubmit = (e, inputValue, addTodo, setInputValue) => {
-  e.preventDefault();
-  if (!validateTodo(inputValue)) {
+/**
+ * 폼 제출 핸들러 함수
+ * @param {Event} event - 폼 제출 이벤트
+ * @param {string} todoText - 입력된 할 일 텍스트
+ * @param {Function} addTodo - 할 일을 추가하는 함수
+ * @param {Function} resetInput - 입력 필드를 초기화하는 함수
+ */
+const handleSubmit = (event, todoText, addTodo, resetInput) => {
+  event.preventDefault();
+  if (!validateTodo(todoText)) {
     alert("할 일을 입력하세요.");
     return;
   }
-  addTodo(inputValue);
-  setInputValue("");
+  addTodo(todoText);
+  resetInput("");
 };
 
 export default handleSubmit;
