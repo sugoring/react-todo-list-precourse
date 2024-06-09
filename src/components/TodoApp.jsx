5e4fb4c797df3bf1c1f2b06ffe43a1dedf8309d6
yuni <hys990730@gmail.com>
2024-06-09 21:31:09 +0900

    refactor: TodoContainer 내부 로직을 TodoApp 컴포넌트로 이동

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import TodoFilter from "./TodoFilter";
+import TodoStats from "./TodoStats";
+import useTodos from "../hooks/useTodos";
+
+const TodoApp = () => {
+  const {
+    todos,
+    handleAddTodo,
+    handleToggleComplete,
+    handleDeleteTodo,
+    handleSetFilter,
+    filter,
+    FILTERS,
+  } = useTodos();
+
+  return (
+    <div>
+      <TodoForm addTodo={handleAddTodo} />
+      <TodoFilter
+        filter={filter}
+        FILTERS={FILTERS}
+        handleSetFilter={handleSetFilter}
+      />
+      <TodoList
+        todos={todos}
+        toggleComplete={handleToggleComplete}
+        deleteTodo={handleDeleteTodo}
+      />
+      <TodoStats todos={todos} />
+    </div>
+  );
+};
+
+export default TodoApp;
--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,35 +1,11 @@
 import React from "react";
-import TodoForm from "./TodoForm";
-import TodoList from "./TodoList";
-import TodoFilter from "./TodoFilter";
-import TodoStats from "./TodoStats";
-import useTodos from "../hooks/useTodos";
+import TodoApp from "./TodoApp";
 
 const TodoContainer = () => {
-  const {
-    todos,
-    handleAddTodo,
-    handleToggleComplete,
-    handleDeleteTodo,
-    handleSetFilter,
-    filter,
-    FILTERS,
-  } = useTodos();
-
   return (
     <div>
-      <TodoForm addTodo={handleAddTodo} />
-      <TodoFilter
-        filter={filter}
-        FILTERS={FILTERS}
-        handleSetFilter={handleSetFilter}
-      />
-      <TodoList
-        todos={todos}
-        toggleComplete={handleToggleComplete}
-        deleteTodo={handleDeleteTodo}
-      />
-      <TodoStats todos={todos} />
+      <h1>할 일 목록</h1>
+      <TodoApp />
     </div>
   );
 };
