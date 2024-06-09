6e42d04b19236e96a2c04c13aaf5cb28f4642dc4
yuni <hys990730@gmail.com>
2024-06-10 00:39:58 +0900

    refactor: useFilteredTodos 훅을 한 가지 일만 하도록 분리
    - 필터링 로직을 관리하는 filterTodos 함수 추가 (todoUtils/filterTodos.js)

--- src/hooks/useFilteredTodos.js
+++ src/hooks/useFilteredTodos.js
@@ -1,19 +1,12 @@
 import { useMemo } from "react";
-import FILTERS from "../todoUtils/filters";
+import filterTodos from "../todoUtils/filterTodos";
 
 // 필터를 적용하여 할 일 목록을 반환하는 훅
 const useFilteredTodos = (todos, filter) => {
-  const filteredTodos = useMemo(() => {
-    switch (filter) {
-      case FILTERS.ACTIVE:
-        return todos.filter((todo) => !todo.completed);
-      case FILTERS.COMPLETED:
-        return todos.filter((todo) => todo.completed);
-      default:
-        return todos;
-    }
-  }, [todos, filter]);
-
+  const filteredTodos = useMemo(
+    () => filterTodos(todos, filter),
+    [todos, filter]
+  );
   return filteredTodos;
 };
 
