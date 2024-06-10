e424b9a289abb745bac543b00d63c00b0c744f7e
yuni <hys990730@gmail.com>
2024-06-10 10:48:09 +0900

    design: todoApp의 스타일 추가

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,16 +1,13 @@
-/* main.css */
-body {
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  background-color: #e6e6e6; /* 전체 배경 색상 */
-}
+import React from "react";
+import TodoContainer from "./components/todoApp/todoContainer";
+import "./Main.css";
 
-.container {
-  max-width: 800px;
-  margin: 20px auto; /* 화면 중앙에 위치하도록 설정 */
-  padding: 20px;
-  background-color: #ffffff; /* 컨테이너 배경 색상 */
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
+const Main = () => {
+  return (
+    <div className="container">
+      <TodoContainer />
+    </div>
+  );
+};
+
+export default Main;
--- src/components/todoApp/todoApp.jsx
+++ src/components/todoApp/todoApp.jsx
@@ -1,9 +1,11 @@
+// src/components/todoApp/todoApp.jsx
 import React from "react";
 import TodoForm from "../todoForm/todoForm";
 import TodoList from "../todoList/todoList";
 import TodoFilter from "../todoFilter/todoFilter";
 import TodoStats from "./todoStats";
 import useTodos from "../../hooks/todos/useTodos";
+import "./todoApp.css";
 
 // 할 일 앱 컴포넌트: 할 일 입력 폼, 할 일 필터, 할 일 목록, 할 일 통계를 표시하는 앱
 const TodoApp = () => {
@@ -19,17 +21,17 @@ const TodoApp = () => {
   } = useTodos();
 
   return (
-    <div>
+    <div className="todo-app">
       <TodoForm addTodo={handleAddTodo} />
-      <TodoFilter
-        filter={filter}
-        FILTERS={FILTERS}
-        handleSetFilter={handleSetFilter}
-      />
       <TodoList
         todos={todos}
         toggleComplete={handleToggleComplete}
         deleteTodo={handleDeleteTodo}
+      />
+      <TodoFilter
+        filter={filter}
+        FILTERS={FILTERS}
+        handleSetFilter={handleSetFilter}
       />
       <TodoStats todos={todos} allTodos={allTodos} filter={filter} />
     </div>
