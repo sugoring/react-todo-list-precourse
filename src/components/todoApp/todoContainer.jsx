44c55ff58cf61ad9dec47eac70732f6f42b57d9b
yuni <hys990730@gmail.com>
2024-06-10 10:41:00 +0900

    design: todoContainer의 container 스타일 추가

--- src/components/todoApp/todoContainer.jsx
+++ src/components/todoApp/todoContainer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import TodoApp from "./todoApp";
+import "./todoContainer.css";
 
-// 할 일 컨테이너 컴포넌트: 할 일 목록을 표시하고 추가하는 기능을 제공하는 컨테이너
 const TodoContainer = () => {
   return (
-    <div>
-      <h1>할 일 목록</h1>
+    <div className="todo-container">
+      <h1>todos</h1>
       <p>Enter 키나 추가 버튼을 사용하여 할 일을 목록에 추가하세요.</p>
       <TodoApp />
+      <p className="footer">Created by [강원대 FE_허윤수]</p>
     </div>
   );
 };
